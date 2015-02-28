f5b2f94bc968389e647aee58e52861993f5010f8
Alex Fernández <alex.fernandez@mediasmart.es>
2015-02-28 12:28:34 +0100

    More connections for wrk.

--- bash/wrk.sh
+++ bash/wrk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Alex 2015-02-22: test using wrk
 
-wrk -c 10 -t 10 http://localhost:55501/ -s sample/caller.lua -d 10
+wrk -c 20 -t 10 http://localhost:55501/ -s sample/caller.lua -d 10
 
